a468023539c2a4ab2b7e7a9d20c48e621c38f739
Owenyx <ow321rowney@gmail.com>
2025-02-17 20:36:11 -0700

    lots of macro stuff

--- test_macro.py
+++ test_macro.py
@@ -1,169 +1,262 @@
 from time import sleep, time
+from threading import Thread
 import numpy as np
 from pynput import keyboard, mouse
 from pynput.keyboard import Key, KeyCode, Controller as KeyboardController
 from pynput.mouse import Button, Controller as MouseController
+import threading
+import ctypes
+from ctypes import wintypes
+
+user32 = ctypes.WinDLL('user32', use_last_error=True)
 
 class Macro:
     def __init__(self):
 
-        ''' Variables for tracking the macro '''
-        self.macro_frequency = 0 # This will be the frequency of the macro
-        self.macro_state = 0 # This will be the state of the macro (0 = off, 1 = on)
-
         ''' Macro '''
+        self.__macro_repeat_delay = 1 # This will be the delay between each repeat of the macro
         self.__inputs = [] # Inputs that the macro will execute
         self.__last_input_time = 0 # Time of the last input
         self.__record_delays = False
         self.__recording = False
+        self.__end_input_key = Key.esc # Key to end a recording, default is esc
+        self.__executing = False
+        self.__absolute_mouse_coords = False # If true, the mouse will move to the absolute coordinates 
+                                                      # recorded instead of relative to the current position
+        self.__block_input_when_executing = True # If true, user input will be blocked when the macro is executing
+        self.keep_initial_position = False # If true, the macro will reset the mouse to where it was at the start of recording
 
-    def enter_input(self, input, delay):
-        self.__inputs.append(input)
+        ''' Controllers '''
+        self.__keyboard = KeyboardController()
+        self.__mouse = MouseController()
+
+        ''' Listeners '''
+        # When recording is started, the callbacks for recording will be configured
+        self.__keyboard_listener = keyboard.Listener(
+            on_press=self.on_event_ignore,
+            on_release=self.on_event_ignore
+        )
+        self.__mouse_listener = mouse.Listener(
+            on_move=self.on_event_ignore,
+            on_click=self.on_event_ignore,
+            on_scroll=self.on_event_ignore
+        )
+
+    @property
+    def recording(self):
+        return self.__recording
+    
+    @property
+    def inputs(self):
+        return self.__inputs
+    
+    @property
+    def executing(self):
+        return self.__executing
+
 
     def remove_input(self, index):
         self.__inputs.pop(index)
 
-    def run_macro(self):
-        while(1):
-            for i in range(len(self.__inputs)): # For each input, execute the delay and input
-                input = self.__inputs[i]
-                self.execute_input(input)
 
-            sleep(self.__macro_frequency)
+    def start_macro(self, n=1): # n is the number of times the macro will be executed
+                                # If n is negative, the macro will be executed until stop_macro is called
+        self.__n = n
+        self.__executing = True
+        if self.__block_input_when_executing:
+            self.block_input()  # Block input when macro starts
+        self.__macro_thread = Thread(target=self.__run_macro, daemon=True)
+        self.__macro_thread.start()   
+
+    def __run_macro(self):
+        while(self.__executing):
+            # Execute the macro
+            for i in range(len(self.__inputs)):
+                if not self.__executing:  # Check if we should stop
+                    return
+                self.__inputs[i]()
+
+            self.__n -= 1
+            # If repeats remain, delay and repeat
+            if self.__n != 0:
+                sleep(self.__macro_repeat_delay)
+            else:
+                self.__executing = False
+                self.unblock_input()  # Restore input when macro stops
+                return
 
 
-    ''' Input execution '''
-    def execute_input(self, input):
-        # Executes an individual delay and input
-        # extract input type and parameters from input string
-        # The notation used for inputs is at the top of the file
-        input_type = input.split('_')[0]
-        input_params = input.split('_')[1:]
-        
-        if input_type == 'kp':
-            self.keyboard.press(input_params[0])
-        elif input_type == 'kr':
-            self.keyboard.release(input_params[0])
-        elif input_type == 'mm':
-            self.mouse.move(input_params[0], input_params[1])
+    def stop_macro(self):
+        self.__executing = False
+        self.unblock_input()  # Restore input when macro stops
+        self.__macro_thread.join() # Might hang if waiting for a delay execution to finish
+
 
     ''' Input recording '''
     def record_basic_input(self):
         # Records basic input such as key presses and mouse clicks
+        # Only ignores mouse movements
         self.__record_delays = False
         self.__inputs = []
-        self.keyboard = KeyboardController(self.on_press_record, self.on_release_record)
-        self.mouse = MouseController(self.on_move_ignore, self.on_click_record, self.on_scroll_record)
-        self.keyboard_listener.start()
-        self.mouse_listener.start()
-        self.__recording = True
+        self.start_recording(mouse_move=False)
         while self.__recording:
             sleep(0.1)
             if len(self.__inputs) > 0:
-                self.end_input()
-        self.__inputs = self.__inputs[0] # Only keep the first input
+                self.end_recording()
+        self.__inputs = [self.__inputs[0]] # Only keep the first input
         # TODO: make it easy to optionally also record the release of the input
         
-
     def record_basic_sequence(self):
         # Records a sequence of basic inputs
+        # Only ignores mouse movements
         self.__record_delays = True
         self.__last_input_time = 0
         self.__inputs = []
-        self.keyboard = KeyboardController(self.on_press_record, self.on_release_record)
-        self.mouse = MouseController(self.on_move_ignore, self.on_click_record, self.on_scroll_record)
-        self.keyboard_listener.start()
-        self.mouse_listener.start()
-        self.__recording = True
+        self.start_recording(mouse_move=False)
 
     def record_full_sequence(self):
         # Records basic sequence as well as all mouse movements
-        # Should add a delay before recording to let the user get ready
+        # TODO: Should add a delay before recording to let the user get ready
         self.__record_delays = True
         self.__last_input_time = 0
         self.__inputs = []
-        self.keyboard = KeyboardController(self.on_press_record, self.on_release_record)
-        self.mouse = MouseController(self.on_move_record, self.on_click_record, self.on_scroll_record)
-        self.keyboard_listener.start()
-        self.mouse_listener.start()
-        self.__recording = True
+        self.start_recording()
 
     # This function will be called when an input is detected and adds a delay action to the inputs list if recording delays
     def input_detected(self):
         if self.__record_delays:
-            def delay_action():
-                sleep(time.time() - self.__last_input_time)
-            self.__last_input_time = time.time()
-            self.__inputs.append(delay_action)
+            current_time = time()  # Get current time
+            dt = current_time - self.__last_input_time
+            if dt > 0:
+                def delay_action():
+                    sleep(dt)
+                delay_action.type = 'delay_{delay}_seconds'.format(delay=dt)
+                self.__inputs.append(delay_action)
+            self.__last_input_time = current_time  # Store for next delay
+
+    def start_recording(self, kb_press=True, kb_release=True, mouse_move=True, mouse_click=True, mouse_scroll=True):
+        # The 5 parameters are bools that determine if the event should be recorded
+        self.__keyboard_listener.on_press = self.on_press_record if kb_press else self.on_event_ignore
+        self.__keyboard_listener.on_release = self.on_release_record if kb_release else self.on_event_ignore
+        self.__mouse_listener.on_move = self.on_move_record if mouse_move else self.on_event_ignore
+        self.__mouse_listener.on_click = self.on_click_record if mouse_click else self.on_event_ignore
+        self.__mouse_listener.on_scroll = self.on_scroll_record if mouse_scroll else self.on_event_ignore
+        self.__recording = True
+
+        if self.keep_initial_position:
+            # Store initial position
+            self.__initial_position = self.__mouse.position
+            
+            def reset_mouse_position():
+                self.__mouse.position = self.__initial_position
+            
+            reset_mouse_position.type = 'reset_mouse_position'
+            self.__inputs.append(reset_mouse_position)
+
+        self.__keyboard_listener.start()
+        self.__mouse_listener.start()
     
-    def end_input(self):
-        # Stop both listeners
-        self.keyboard_listener.stop()
-        self.mouse_listener.stop()
-        self.__recording = False
+    def end_recording(self):
+        self.__recording = False  # Set this first
+        # Just stop the listeners
+        self.__keyboard_listener.stop()
+        self.__mouse_listener.stop()
+        
+        # If delays were recorded, remove the initial delay
+        if self.__record_delays:
+            # Find and remove first delay action
+            for i, action in enumerate(self.__inputs):
+                if action.type.startswith('delay'):
+                    self.__inputs.pop(i)
+                    break
+
 
     ''' Event listeners '''
     # Each event listener will create a replay function for that specific event and add it to the inputs list
     def on_press_record(self, key):
+        self.input_detected()
         # Checks if the end input key was pressed
-        if key == self.end_input_key:
-            self.end_input()
+        if key == self.__end_input_key:
+            self.end_recording()
+            return
 
         def replay_action():
-            self.input_detected()
             if isinstance(key, KeyCode):
                 # Regular character key
-                self.keyboard.press(key.char)
+                self.__keyboard.press(key.char)
             else:
                 # Special key (like shift, ctrl, etc)
-                self.keyboard.press(key)
+                self.__keyboard.press(key)
+
+        # Add metadata to see what the event was
+        replay_action.type = 'key_press_' + str(key)
+
+        self.__inputs.append(replay_action)
         
     def on_release_record(self, key):
+        self.input_detected()
         def replay_action():
-            self.input_detected()
             if isinstance(key, KeyCode):
                 # Regular character key
-                self.keyboard.release(key.char)
+                self.__keyboard.release(key.char)
             else:
                 # Special key (like shift, ctrl, etc)
-                self.keyboard.release(key)
+                self.__keyboard.release(key)
+
+        # Add metadata to see what the event was
+        replay_action.type = 'key_release_' + str(key)
+
+        self.__inputs.append(replay_action)
             
     def on_move_record(self, x, y):
-        def replay_action():
-            self.input_detected()
-            self.mouse.move(x, y)
+        self.input_detected()
+        if self.__absolute_mouse_coords:
+            def replay_action():
+                self.__mouse.position = (x, y)
+        else:
+            # Move the mouse relative to the current position
+            dx = x - self.__mouse.position[0]
+            dy = y - self.__mouse.position[1]
+            def replay_action():
+                self.__mouse.move(dx, dy)
+
+        # Add metadata to see what the event was
+        replay_action.type = 'mouse_move'
+
+        self.__inputs.append(replay_action)
         
     def on_click_record(self, x, y, button, pressed):
+        self.input_detected()
         if pressed:
             def replay_action():
-                self.input_detected()
-                self.mouse.click(button, x, y)
+                if self.__absolute_mouse_coords:
+                    self.__mouse.position = (x, y)
+
+                self.__mouse.click(button)
         else:
             def replay_action():
-                self.mouse.release(button)
+                self.__mouse.release(button)
+
+        # Add metadata to see what the event was
+        replay_action.type = 'mouse_{click}_{button}'.format(click='press' if pressed else 'release', button=button)
+
+        self.__inputs.append(replay_action)
         
     def on_scroll_record(self, x, y, dx, dy):
+        self.input_detected()
         def replay_action():
-            self.input_detected()
-            self.mouse.scroll(x, y, dx, dy)
+            self.__mouse.scroll(dx, dy)
 
-    # These event listeners will be used to ignore events instead, so that recorded events can be configurable
-    def on_press_ignore(self, key):
-        # Still checks if the end input key was pressed
-        if key == self.end_input_key:
-            self.end_input()
+        # Add metadata to see what the event was
+        replay_action.type = 'mouse_scroll_{direction}'.format(direction='down' if dy < 0 else 'up')
 
-    def on_release_ignore(self, key):
-        pass
+        self.__inputs.append(replay_action)
 
-    def on_move_ignore(self, x, y):
-        pass
-
-    def on_click_ignore(self, x, y, button, pressed):
-        pass
-
-    def on_scroll_ignore(self, x, y, dx, dy):
-        pass   
+    def on_event_ignore(self, *args):
+        # Generic function to ignore any event
+        # Still checks if the end input key was pressed,
+        if args[0] == self.__end_input_key: # args[0] would be the key pressed if it was called for a key press event
+            self.stop_macro()
 
     ''' Macro management '''
     def save_macro(self):
@@ -172,3 +265,41 @@ class Macro:
     def load_macro(self):
         pass
 
+
+    ''' Input blocking '''
+
+    def block_input(self, block=True):
+        """Block/unblock all input except from macro"""
+        # 0 = unblock, 1 = block
+        result = user32.BlockInput(wintypes.BOOL(block))
+        if not result:
+            error = ctypes.get_last_error()
+            raise Exception(f'Failed to {"block" if block else "unblock"} input: {error}')
+
+    def unblock_input(self):
+        self.block_input(False)
+
+
+if __name__ == '__main__':
+    macro = Macro()
+    macro.keep_initial_position = True
+    macro.record_full_sequence()
+    print('Recording started')
+    while macro.recording:
+        sleep(0.1)
+    print('Recording stopped')
+    
+    # Print recorded inputs
+    print("\nRecorded inputs:")
+    for i, action in enumerate(macro.inputs): 
+        try:
+            print(f"{i}: {action.type}")
+        except:
+            print(f"{i}: {action}")
+
+    
+    macro.start_macro(n=1)
+    print('Replaying macro')
+    while macro.executing:
+        sleep(0.1)
+    print('Replaying stopped')
